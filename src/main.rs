a1be6cda3ac71ca5e74d9e08f17e3d895bf37373
Max Verevkin <me@maxverevkin.tk>
2021-06-27 23:11:38 +0300

    remove `text_io` dependecy

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,40 @@
-use crate::tic_tac_toe::game_board::GameBoard;
-use text_io::try_read;
+mod tic_tac_toe;
 
+use crate::tic_tac_toe::game_board::GameBoard;
+
+use std::error::Error;
 use std::io::{stdin, stdout, Write};
 use termion::event::{Event, Key, MouseEvent};
 use termion::input::{MouseTerminal, TermRead};
 use termion::raw::IntoRawMode;
 
-/// Implements game state for a game of tic-tac-toe
-mod tic_tac_toe;
-
 /// Create a `GameBoard` and simulate a game of tic-tac-toe
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     print!("Input board size (default is 3, min is 2): ");
-    stdout().flush().unwrap();
-    let board_size = try_read!("{}\n").unwrap_or(3).max(2);
+    stdout().flush()?;
+
+    let mut buf = String::new();
+    stdin().read_line(&mut buf)?;
+    let mut game_board = GameBoard::new(buf.trim().parse()?);
 
     // Init termion
-    let mut stdout = MouseTerminal::from(stdout().into_raw_mode().unwrap());
+    let mut stdout = MouseTerminal::from(stdout().into_raw_mode()?);
 
-    let mut game_board = GameBoard::new(board_size);
-    game_board.print_board(&mut stdout).unwrap();
-
-    'game: while !game_board.game_over() {
+    'game: loop {
+        game_board.print_board(&mut stdout)?;
+        if game_board.game_over() {
+            break;
+        }
+        // Handle keyboard/mouse events
         for c in stdin().events() {
             match c.unwrap() {
-                Event::Key(Key::Char('q')) => return,
+                // Press `q` to quit
+                Event::Key(Key::Char('q')) => return Ok(()),
                 Event::Mouse(MouseEvent::Press(_, x, y)) if x % 2 == 0 => {
                     if game_board
                         .mark_cell((x / 2 - 1) as usize, y as usize - 1)
                         .is_some()
                     {
-                        game_board.print_board(&mut stdout).unwrap();
                         continue 'game;
                     }
                 }
@@ -39,7 +43,11 @@ fn main() {
         }
     }
 
+    // Deinit termion
     drop(stdout);
+
     println!();
     game_board.print_game_over_message();
+
+    Ok(())
 }
